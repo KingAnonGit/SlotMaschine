8836c8e2b6c16b5d12aa726e187dda9425222827
PascalKu <kutpasc@gmail.com>
2017-08-22 15:54:30 +0200

    Updated.

--- src/main/java/de/kinganon/slotmaschine/SlotMaschine.java
+++ src/main/java/de/kinganon/slotmaschine/SlotMaschine.java
@@ -1,23 +1,27 @@
 package de.kinganon.slotmaschine;
 
+import lombok.Getter;
 import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import de.kinganon.slotmaschine.cache.Vars;
+import de.kinganon.slotmaschine.cache.Variables;
 import de.kinganon.slotmaschine.events.inventoryClickEvent;
 import de.kinganon.slotmaschine.events.InteractEvent;
 import de.kinganon.slotmaschine.events.JoinEvent;
 
 public class SlotMaschine extends JavaPlugin {
     
-    public static Plugin plugin = null;
+    @Getter
+    private static SlotMaschine slotMaschine;
+    
+    public SlotMaschine() {
+        slotMaschine = this;
+    }
     
     public void onEnable() {
-        plugin = this;
         Bukkit.getPluginManager().registerEvents(new inventoryClickEvent(), this);
         Bukkit.getPluginManager().registerEvents(new JoinEvent(), this);
         Bukkit.getPluginManager().registerEvents(new InteractEvent(), this);
-        Bukkit.getConsoleSender().sendMessage(Vars.prefix + "KnockIt wurde erfolgreich Gestartet!");
+        Bukkit.getConsoleSender().sendMessage(Variables.prefix + "KnockIt wurde erfolgreich Gestartet!");
     }
 }
